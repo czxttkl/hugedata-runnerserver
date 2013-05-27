4c95573a9c82c074942dde28c9041aa4f38db235
czxttkl <czxttkl@gmail.com>
2013-05-27 16:08:21 +0800

    加入TestAnalyzer javadoc
    to do list:
    加入best practice
    手机队列改进
    daemon线程监测新加入设备
    tasklistenerhandler socket write错误
    resultbundle中文资源解码

--- src/com/czxttkl/hugedata/analyze/TestAnalyzer.java
+++ src/com/czxttkl/hugedata/analyze/TestAnalyzer.java
@@ -25,17 +25,18 @@ public abstract class TestAnalyzer {
 			"ResultBundle", Locale.CHINA);
 	public TaskListenerHandler taskListenerHandler;
 	public Document doc;
-    public HashMap<String, String> publicMetrics;
-	
+	public HashMap<String, String> publicMetrics;
+
 	public volatile boolean notified = false;
-	
+
 	public TestAnalyzer(String resultDirStr,
-			TaskListenerHandler taskListenerHandler, HashMap<String, String> publicMetrics) throws IOException {
-		
+			TaskListenerHandler taskListenerHandler,
+			HashMap<String, String> publicMetrics) throws IOException {
+
 		this.resultDirStr = resultDirStr;
 		this.taskListenerHandler = taskListenerHandler;
 		this.publicMetrics = publicMetrics;
-		
+
 		File html = new File("html/template/index.html");
 		doc = Jsoup.parse(html, "UTF-8");
 		setTitle();
@@ -60,9 +61,13 @@ public abstract class TestAnalyzer {
 		return keyValue;
 	}
 
+	/**
+	 * Analyzer will block after append public metrics until all the results
+	 * have been pulled from the mobile end.
+	 */
 	public synchronized void waitForTestFinish() {
 		try {
-			while(!notified)
+			while (!notified)
 				wait();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
@@ -72,93 +77,95 @@ public abstract class TestAnalyzer {
 	public synchronized void notifyForTestFinish() {
 		notified = true;
 		notify();
-		
+
 	}
 
 	public void appendPublicMetrics() throws IOException {
 		System.out.println("appendPublicMetrics");
-		
-		Element testType= doc.getElementById("testType");
+
+		Element testType = doc.getElementById("testType");
 		testType.text(getStringValue("html.test.type.packettest"));
-		
+
 		Element startTime = doc.getElementById("startTime");
 		startTime.text(publicMetrics.get("StartTime"));
-		
-		Element duration= doc.getElementById("duration");
+
+		Element duration = doc.getElementById("duration");
 		duration.text(publicMetrics.get("Duration"));
-		
-		Element location= doc.getElementById("location");
+
+		Element location = doc.getElementById("location");
 		location.text(publicMetrics.get("Location"));
-		
+
 		Element phoneManufacturer = doc.getElementById("phoneManufacturer");
 		phoneManufacturer.text(publicMetrics.get("PhoneManufacturer"));
-		
-		Element phoneType= doc.getElementById("phoneType");
+
+		Element phoneType = doc.getElementById("phoneType");
 		phoneType.text(publicMetrics.get("PhoneType"));
-		
-		Element platformName= doc.getElementById("platformName");
+
+		Element platformName = doc.getElementById("platformName");
 		platformName.text(publicMetrics.get("PlatformName"));
-		
+
 		Element platformVer = doc.getElementById("platformVer");
 		platformVer.text(publicMetrics.get("PlatformVer"));
-		
+
 		Element network = doc.getElementById("network");
 		network.text(publicMetrics.get("Network"));
-		
+
 		Element ipAddress = doc.getElementById("ipAddress");
 		ipAddress.text(publicMetrics.get("IpAddress"));
-		
+
 		Element primeDns = doc.getElementById("primeDns");
 		primeDns.text(publicMetrics.get("PrimeDns"));
-		
+
 		Element secondaryDns = doc.getElementById("secondaryDns");
 		secondaryDns.text(publicMetrics.get("SecondaryDns"));
-		
+
 	}
 
 	public void setTitle() {
 
 		Element title = doc.getElementById("title");
 		title.text(getStringValue("html.title"));
-		
+
 		Element testTypeTitle = doc.getElementById("testTypeTitle");
 		testTypeTitle.text(getStringValue("html.test.type.title"));
-		
+
 		Element startTimeTile = doc.getElementById("startTimeTitle");
 		startTimeTile.text(getStringValue("html.start.time.title"));
-		
-		Element durationTitle= doc.getElementById("durationTitle");
+
+		Element durationTitle = doc.getElementById("durationTitle");
 		durationTitle.text(getStringValue("html.duration.title"));
-		
-		Element locationTitle= doc.getElementById("locationTitle");
+
+		Element locationTitle = doc.getElementById("locationTitle");
 		locationTitle.text(getStringValue("html.location.title"));
-		
-		Element phoneManufacturerTitle = doc.getElementById("phoneManufacturerTitle");
-		phoneManufacturerTitle.text(getStringValue("html.phone.manufacturer.title"));
-		
-		Element phoneTypeTitle= doc.getElementById("phoneTypeTitle");
+
+		Element phoneManufacturerTitle = doc
+				.getElementById("phoneManufacturerTitle");
+		phoneManufacturerTitle
+				.text(getStringValue("html.phone.manufacturer.title"));
+
+		Element phoneTypeTitle = doc.getElementById("phoneTypeTitle");
 		phoneTypeTitle.text(getStringValue("html.phone.type.title"));
-		
-		Element networkTitle= doc.getElementById("networkTitle");
+
+		Element networkTitle = doc.getElementById("networkTitle");
 		networkTitle.text(getStringValue("html.network.title"));
-		
-		Element platformNameTitle= doc.getElementById("platformNameTitle");
+
+		Element platformNameTitle = doc.getElementById("platformNameTitle");
 		platformNameTitle.text(getStringValue("html.platform.name.title"));
-		
-		Element platformVerTitle= doc.getElementById("platformVerTitle");
+
+		Element platformVerTitle = doc.getElementById("platformVerTitle");
 		platformVerTitle.text(getStringValue("html.platform.ver.title"));
-		
-		Element ipAddressTitle= doc.getElementById("ipAddressTitle");
+
+		Element ipAddressTitle = doc.getElementById("ipAddressTitle");
 		ipAddressTitle.text(getStringValue("html.ip.address.title"));
-		
-		Element primeDnsTitle= doc.getElementById("primeDnsTitle");
+
+		Element primeDnsTitle = doc.getElementById("primeDnsTitle");
 		primeDnsTitle.text(getStringValue("html.prime.dns.title"));
-		
-		Element secondaryDnsTitle= doc.getElementById("secondaryDnsTitle");
+
+		Element secondaryDnsTitle = doc.getElementById("secondaryDnsTitle");
 		secondaryDnsTitle.text(getStringValue("html.secondary.dns.title"));
-		
+
 	}
-	
+
 	public abstract void appendPrivateMetrics() throws UnsatisfiedLinkError,
 			IOException;
 
